0b6b99fd5b5829d5601d2251d4475134d1ec9585
Evan Morrison <evanmorrison@filevine.com>
2021-10-01 16:42:58 -0600

    styling

--- src/components/ButtonRow/ButtonRow.js
+++ src/components/ButtonRow/ButtonRow.js
@@ -3,7 +3,11 @@ import React from 'react';
 import styles from './ButtonRow.module.css';
 
 const ButtonRow = ({ children }) => {
-  return <div className={styles.buttonRow}>{children}</div>;
+  return <div className={styles.buttonRow}>
+  <div className={styles.buttonRowInner}>
+    {children}
+  </div>
+  </div>;
 };
 
 export default ButtonRow;
